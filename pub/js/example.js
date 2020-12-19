f4e4ce5fa54e7c82ac827ba12ddb196be14f4307
ArjunPandher <arjunpandher@gmail.com>
2020-12-19 16:23:12 -0500

    Attempted to fix issue with canvas height calculations

--- pub/js/example.js
+++ pub/js/example.js
@@ -3,8 +3,8 @@ const audioGen2 = new AudioVisGenerator("player2")
 const audioGen3 = new AudioVisGenerator("player3")
 
 const barCanvas = document.getElementById("canvas-bar")
-barCanvas.width = "1000px"
-barCanvas.height = "300px"
+barCanvas.width = "1000"
+barCanvas.height = "300"
 const barCanvasVis = audioGen1.addCanvas(barCanvas, 0, "#000000", "#ffb4a2")
 
 const player1 = document.getElementById("player1")
