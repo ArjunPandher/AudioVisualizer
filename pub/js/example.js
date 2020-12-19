fd8436809720b6c10fe5ec7dd5bf85edbf8e4af3
ArjunPandher <arjunpandher@gmail.com>
2020-12-19 15:45:37 -0500

    Fixed issue with implementation; Updated api.html to reflect that change

--- pub/js/example.js
+++ pub/js/example.js
@@ -6,7 +6,11 @@ const barCanvas = document.getElementById("canvas-bar")
 const barCanvasVis = audioGen1.addCanvas(barCanvas, 0, "#ffb4a2", "#b5838d")
 
 const player1 = document.getElementById("player1")
+let player1HasBeenPlayed = false
 player1.onplay = () => {
-    audioGen1.setUp()
+    if (!player1HasBeenPlayed) {
+        audioGen1.setUp()
+        player1HasBeenPlayed = true
+    }
     audioGen1.animateAllCanvases()
 }
