a73a12a68205dc4cfadb7f37229ef94e95d7f44d
ArjunPandher <arjunpandher@gmail.com>
2020-12-19 19:01:39 -0500

    Small bug fix

--- pub/js/example.js
+++ pub/js/example.js
@@ -18,8 +18,16 @@ player1.onplay = () => {
         player1HasBeenPlayed = true
     }
     audioGen1.animateAllCanvases()
+}
+
+const player2 = document.getElementById("player2")
+let player2HasBeenPlayed = false
+player1.onplay = () => {
+    if (!player2HasBeenPlayed) {
+        audioGen2.setUp()
+        player2HasBeenPlayed = true
+    }
     audioGen2.animateAllElements()
-    console.log("bruh")
 }
 
 $("li").on("click", function() {
